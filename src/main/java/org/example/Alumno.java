bce98483b6b0c10ed6c045539c2664312bb77e2b
Pablo Jiménez Juárez <pablojjuarez567@gmail.com>
2023-03-06 11:49:59 +0100

    arreglado cosas

--- src/main/java/org/example/Alumno.java
+++ src/main/java/org/example/Alumno.java
@@ -12,15 +12,10 @@ public class Alumno {
 
     @Id
     Long id;
-    @Basic
     String nombre;
-    @Basic
     String telefono;
-    @Basic
     String email;
-    @Basic
     Double notaAD;
-    @Basic
     Double notaDI;
 
 }
